f4eb43a870f76e40cfde98d139979fdad973619b
Matti Hallamaa <matti.hallamaa@student.careeria.fi>
2024-01-22 19:21:13 +0200

    Add project files.

---
+++ NWRestAPI/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NWRestAPI.Models;
+
+namespace NWRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        //Alustetaan tietokantayhteys
+        NorthwindContext db = new NorthwindContext();
+
+        [HttpGet]
+
+        //Hakee kaikki asiakkaat
+        public ActionResult GetAllCustomers()
+        {
+            var asiakkaat = db.Customers.ToList();
+            return Ok(asiakkaat);
+        }
+    }
+}
