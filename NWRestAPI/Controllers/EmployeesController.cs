65e28280e67e5875c47baac9a7e4e49bdc6b9a73
Matti Hallamaa <matti.hallamaa@student.careeria.fi>
2024-01-23 19:52:39 +0200

    lisätty products & eployees controllerit CRUD-toiminnoilla

---
+++ NWRestAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using NWRestAPI.Models;
+
+namespace NWRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        //Dependency injektio
+        private NorthwindContext db;
+
+        public EmployeesController(NorthwindContext dbparametri)
+        {
+            db = dbparametri;
+        }
+
+        [HttpGet("{id}")]
+
+        //Hakee yhen asiakkaan pääavaimella
+        public ActionResult GetEmployeeById(int id)
+        {
+            try
+            {
+                var employee = db.Employees.Find(id);
+                if (employee != null)
+                {
+                    return Ok(employee);
+                }
+                else
+                {
+                    return BadRequest($"Employee not found with the id {id}.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetAllEployees()
+        {
+            try
+            {
+                var employees = db.Employees.ToList();
+                return Ok(employees);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException);
+            }
+        }
+
+        [HttpGet("employeename/{ename}")]
+
+        public ActionResult GetByName(string ename)
+        {
+            try
+            {
+                var emp = db.Employees.Where(e => e.LastName.Contains(ename));
+
+                return Ok(emp);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+
+        [HttpPost]
+
+        public ActionResult AddNew([FromBody] Employee emp)
+        {
+            try
+            {
+                db.Employees.Add(emp);
+                db.SaveChanges();
+                return Ok($"Added new employee {emp.LastName} , {emp.FirstName}.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException);
+            }
+        }
+
+        [HttpPut("{id}")]
+
+        public ActionResult EditEmployee(int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                var emp = db.Employees.Find(id);
+
+                if (emp != null)
+                {
+                    emp.LastName = employee.LastName;
+                    emp.FirstName = employee.FirstName;
+                    emp.Address = employee.Address;
+                    emp.City = employee.City;
+                    emp.Orders = employee.Orders;
+                    emp.TitleOfCourtesy = employee.TitleOfCourtesy;
+                    emp.Country = employee.Country;
+                    emp.PostalCode = employee.PostalCode;
+                    emp.Photo = employee.Photo;
+                    emp.PhotoPath = employee.PhotoPath;
+                    emp.BirthDate = employee.BirthDate;
+                    emp.HireDate = employee.HireDate;
+                    emp.Extension = employee.Extension;
+                    emp.HomePhone = employee.HomePhone;
+                    emp.Notes = employee.Notes;
+                    emp.Region = employee.Region;
+                    emp.ReportsTo = employee.ReportsTo;
+                    emp.Title = employee.Title;
+                   
+
+                    db.SaveChanges();
+                    return Ok("Edited employee " + employee.LastName + ", " + employee.FirstName);
+                }
+
+                return NotFound("Employee not found with the id " + id);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+
+
+
+
+        }
+        [HttpDelete("{id}")]
+
+        //Asiakkaan poistaminen
+
+        public ActionResult DeleteEmployeeById(int id)
+        {
+            try
+            {
+                var employee = db.Employees.Find(id);
+
+                if (employee != null)
+                {
+                    db.Employees.Remove(employee);
+                    db.SaveChanges();
+                    return Ok($"Employee {employee.LastName}, {employee.FirstName} removed");
+                }
+                else
+                {
+                    return NotFound("Employee with an id" + id + "was no found");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException);
+            }
+
+
+        }
+    }
+}
---
+++ NWRestAPI/Controllers/ProductsController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using NWRestAPI.Models;
+
+namespace NWRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private NorthwindContext db;
+
+        public ProductsController(NorthwindContext dbparametri)
+        {
+            db = dbparametri;
+        }
+
+        [HttpGet("{id}")]
+
+        //Hakee yhden tuotteen pääavaimella
+        public ActionResult GetProductById(int id)
+        {
+            try
+            {
+                var product = db.Products.Find(id);
+                if (product != null)
+                {
+                    return Ok(product);
+                }
+                else
+                {
+                    return BadRequest($"Product not found with the id {id}.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException);
+            }
+        }
+
+
+        //Kaikkien tuotteiden haku
+        [HttpGet]
+        public ActionResult GetAllProducts()
+        {
+            try
+            {
+                var products = db.Products.ToList();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException);
+            }
+        }
+
+
+        //Tuotteen haku nimen osalla
+        [HttpGet("productname/{pname}")]
+
+        public ActionResult GetByName(string pname)
+        {
+            try
+            {
+                var products = db.Products.Where(p => p.ProductName.Contains(pname));
+
+                return Ok(products);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+
+
+        //Tuotteen lisääminen
+        [HttpPost]
+
+        public ActionResult AddNew([FromBody] Product product)
+        {
+            try
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return Ok($"Added new product {product.ProductName}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException);
+            }
+        }
+
+
+        //Tuotteen muokkaaminen
+        [HttpPut("{id}")]
+
+        public ActionResult EditProduct(int id, [FromBody] Product product)
+        {
+            try
+            {
+                var pro = db.Products.Find(id);
+
+                if (pro != null)
+                {
+                    pro.ProductName = product.ProductName;
+                    pro.UnitPrice = product.UnitPrice;
+                    pro.QuantityPerUnit = product.QuantityPerUnit;
+                    pro.Discontinued = product.Discontinued;
+                    pro.SupplierId = product.SupplierId;
+                    pro.CategoryId = product.CategoryId;
+                    pro.UnitsOnOrder = product.UnitsOnOrder;
+                    pro.ReorderLevel = product.ReorderLevel;
+                    pro.ImageLink = product.ImageLink;
+
+                  
+
+
+                    db.SaveChanges();
+                    return Ok("Edited product " + product.ProductName);
+                }
+
+                return NotFound("Product not found with the id " + id);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+
+
+
+
+        }
+        [HttpDelete("{id}")]
+
+        //Tuotteen poistaminen
+
+        public ActionResult DeletePoductById(int id)
+        {
+            try
+            {
+                var product = db.Products.Find(id);
+
+                if (product != null)
+                {
+                    db.Products.Remove(product);
+                    db.SaveChanges();
+                    return Ok($"Product {product.ProductName} removed");
+                }
+                else
+                {
+                    return NotFound("Product with an id" + id + "was no found");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException);
+            }
+
+
+        }
+    }
+}
